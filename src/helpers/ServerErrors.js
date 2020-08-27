300d60dda177a6bdc952493844d4574d86d9c96a
d0177381@gmail.com <>
2020-08-28 00:26:54 +0800

    [Add] ServerErrors file in helpers folder for 5XX http status code

---
+++ src/helpers/ServerErrors.js
@@ -0,0 +1,25 @@
+const httpStatus = require('http-status');
+
+class ExtendableError extends Error {
+  constructor(message, status, isPublic, code) {
+    super(message);
+    this.message = message;
+    this.name = this.constructor.name;
+    this.status = status;
+    this.isPublic = isPublic;
+    this.code = code;
+    this.isOperational = true; // This is required since bluebird 4 doesn't append it anymore.
+    Error.captureStackTrace(this, this.constructor.name);
+  }
+}
+
+class AxiosGetMethodError extends ExtendableError {
+  constructor(message = 'Axios GET 請求失敗', status = 50000, isPublic = true, code = httpStatus.INTERNAL_SERVER_ERROR) {
+    super(message, status, isPublic, code);
+    // this.name = 'LoginError';
+  }
+}
+
+module.exports = {
+  AxiosGetMethodError
+};
