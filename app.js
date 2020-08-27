515a48fc7a2a36504e1869c445828942a8ebd39d
d0177381@gmail.com <>
2020-08-28 00:19:37 +0800

    [Add] scheduler router to test

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.json('ok');
-});
+// Routers
+app.use('/scheduler', require('./src/routers/schedulers'));
 
 const port = process.env.PORT;
 app.listen(port, function()  {
   console.log(`Love Never Stray on port ${port}`);
 });
+
 module.exports = app;
