451970eb9c43e017b1edcc48cbe1ed3ddbb0778a
d0177381@gmail.com <>
2020-08-29 21:44:00 +0800

    [Rename] setAdoptionsFromCOA to setPetDataFromCOA

--- src/routers/schedulers.js
+++ src/routers/schedulers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/COA_data', require('../schedulers/setAdoptionsFromCOA'));
+router.get('/COA_data', require('../schedulers/setPetDataFromCOA'));
 
 module.exports = router;
