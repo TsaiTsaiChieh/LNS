f5c76c51cee13cc73a682d8235599e88ed57c996
d0177381@gmail.com <>
2020-08-30 20:29:37 +0800

    [Add] view engine and static path

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require('express');
 const app = express();
 const Haven = require('domain-haven');
 
-
 app.use(Haven.haven());
+app.use(express.static('./src/public'));
 // Routers
+app.use(require('./src/routers/index'));
 app.use('/scheduler', require('./src/routers/schedulers'));
+// view engine
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 const port = process.env.PORT;
-app.listen(port, function()  {
-  console.log(`Love Never Stray on port ${port}`);
+app.listen(port, function() {
+    console.log(`Love Never Stray on port ${port}`);
 });
 
 module.exports = app;
